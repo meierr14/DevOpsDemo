aef8a5b47c6af939279d95a4995b49c5eb7460c6
Renzo Meier <meierr14@students.zhaw.ch>
2023-05-23 09:57:07 +0200

    weiterer Test um die Methode setId() zu testen

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
@@ -1,6 +1,7 @@
 package ch.zhaw.iwi.devops.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.description;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,21 @@ public class ToDoTest {
         assertEquals("title", toDo1.getTitle());
         assertEquals("description", toDo1.getDescription());
         assertEquals(1, toDo1.getId());
+    }
+
+    @Test
+    public void testToDo2() {
+        var todo2 = new ToDo(2, "test", "test nr 2");
+        assertEquals("test", todo2.getTitle());
+        assertEquals("test nr 2", todo2.getDescription());
+        assertEquals(2, todo2.getId());
+    }
+
+    @Test
+    public void testToDo3(){
+        var todo3 = new ToDo(2, "test3", "ID should be 3");
+        todo3.setId(3);
+        assertEquals(3, todo3.getId());
     }
     
 }
