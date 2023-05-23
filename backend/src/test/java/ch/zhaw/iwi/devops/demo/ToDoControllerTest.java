1e26b20b144be422a43a5a2f77154e06beeebd99
Renzo Meier <meierr14@students.zhaw.ch>
2023-05-23 09:19:39 +0200

    Neuer Test für die Delete-Methode um den JaCoCo Report zu testen

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
@@ -15,4 +15,15 @@ public class ToDoControllerTest {
         assertEquals(1, controller.todo().size());
     }
 
+    @Test
+    public void testDelete(){
+        ToDoController controller = new ToDoController();
+        var todo = new ToDo(1, "t", "d");
+        var todo2 = new ToDo(2, "titel", "description");
+        controller.createTodo(1, todo);
+        controller.createTodo(2, todo2);
+        controller.deleteTodo(1);
+        assertEquals(1, controller.count());
+    }
+
 }
