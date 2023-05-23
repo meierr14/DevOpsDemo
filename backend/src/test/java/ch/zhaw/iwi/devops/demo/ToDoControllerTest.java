ede489164ad8669ec1339dd870e0a7ddb354114d
Renzo Meier <meierr14@students.zhaw.ch>
2023-05-23 10:05:00 +0200

    Neuer Test um die Methode ping() zu testen

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
@@ -34,4 +34,10 @@ public class ToDoControllerTest {
 
     }
 
+    @Test
+    public void testPing(){
+        ToDoController controller = new ToDoController();
+        assertEquals("{ \"status\": \"ok\", \"userId\": \"admin"+ "\", \"languageCode\": \"de\",\"version\": \"0.0.1" + "\"}", controller.ping());
+    }
+
 }
