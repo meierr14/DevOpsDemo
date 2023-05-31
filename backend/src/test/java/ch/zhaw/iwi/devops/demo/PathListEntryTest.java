a1864d5a09490bda765ad84a5e634525ca8ab172
Renzo Meier <meierr14@students.zhaw.ch>
2023-05-31 10:07:36 +0200

    New test for PathListEntry

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/PathListEntryTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/PathListEntryTest.java
@@ -2,6 +2,9 @@ package ch.zhaw.iwi.devops.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import ch.zhaw.iwi.devops.demo.PathListEntry.Key;
@@ -17,5 +20,12 @@ public class PathListEntryTest {
         assertEquals(true, listEntry.isActive());
         assertEquals("green", listEntry.getColor());
         assertEquals("Test1", listEntry.getName());
+    }
+
+    @Test
+    public void test2() {
+        PathListEntry listEntry = new PathListEntry<>();
+        listEntry.setIcon("circle");
+        assertEquals("circle", listEntry.getIcon());
     }
 }
